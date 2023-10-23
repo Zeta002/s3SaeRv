5ed73a96cf4594b5f65b0858ef55f4dd1180e241
Zeta002 <angeloportelli05@gmail.com>
2023-10-23 18:05:27 +0200

    Homepage v1 + font + architecture

---
+++ pages/homepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAE S3 Projet RV</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
+<body>
+    <nav class="main-nav">
+        <div id="pages-container">
+            <a class="pages-link" href="./about">A propos</a>
+            <a class="pages-link" href="./game">Jouer</a>
+            <a class="pages-link" href="./howtoplay">Comment jouer</a>
+        </div>
+        <img src="../images/univ-logo.png" alt="">
+    </nav>
+    <div class="content">
+        <h1>Contenu de la page</h1>
+        <p>Autres éléments...</p>
+    </div>
+</body>
+</html>
