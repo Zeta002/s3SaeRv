72caec61edc6fcc147bdc339bfefb06a308bb1a5
Zeta002 <angeloportelli05@gmail.com>
2023-10-25 09:18:29 +0200

    Upgrade navbar.css + ajout des pages
    Les liens vers les autres pages sont animé au survol et un lien vers le site officiel de l'IUT à étais ajouté au logo de la navbar.
    
    Les pages vierges pour l'instant ont étais ajouter créer et ajouter à la navbar, il faudras faire plus tard un namespace et un routeur.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAE S3 Projet RV</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+<body>
+    <nav class="main-nav">
+        <div id="pages-container">
+            <a class="pages-link" href="../pages/about">A propos</a>
+            <a class="pages-link" href="../pages/game">Jouer</a>
+            <a class="pages-link" href="../pages/howtoplay">Comment jouer</a>
+            <a class="pages-link" href="../pages/credit">Crédit</a>
+        </div>
+        <a href="https://iut.univ-amu.fr/fr">
+            <img id="logo-iut" src="images/univ-logo.png" alt="logo institut universitaire de technologie">
+        </a>
+    </nav>
+    <div class="content">
+        <h1>Contenu de la page</h1>
+        <p>Autres éléments...</p>
+    </div>
+</body>
+</html>
