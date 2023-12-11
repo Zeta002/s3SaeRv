93212d76dd5b7df3c87e7fef37e7f7f816ad9f7b
Zeta002 <angeloportelli05@gmail.com>
2023-12-11 22:27:18 +0100

    Correction indentation

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -1,35 +1,16 @@
 <?php
 
-    namespace App\src\Router;
+namespace App\src\Router;
 
-    class Route
+class Route
+{
+    private $path;
+    private $callable;
+    private $matches;
+
+    public function __construct($path, $callable)
     {
-        private $path;
-        private $callable;
-        private $matches;
-
-        public function __construct($path, $callable)
-        {
-            $this->path = trim($path, '/');
-            $this->callable = $callable;
-        }
-
-        public function match($url)
-        {
-            $url = trim($url, '/');
-            $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path);
-            $regex = "#^$path$#i";
-            if (!preg_match($regex, $url, $matches)) {
-                return false;
-            }
-            array_shift($matches);
-            $this->matches = $matches;
-            return true;
-        }
-
-        public function call()
-        {
-            return call_user_func_array($this->callable, $this->matches);
-        }
-
+        $this->path = trim($path, '/');
+        $this->callable = $callable;
     }
+}
--- src/Router/Router.php
+++ src/Router/Router.php
@@ -1,44 +1,44 @@
 <?php
 
-    namespace App\src\Router;
+namespace App\src\Router;
 
-    use App\src\Controller\Error404Controller;
+use App\src\Controller\Error404Controller;
 
 
-    class Router
+class Router
+{
+    private $url;
+    private $routes = [];
+
+    public function __construct($url)
     {
-        private $url;
-        private $routes = [];
+        $this->url = $url;
+    }
 
-        public function __construct($url)
-        {
-            $this->url = $url;
-        }
+    public function get($path, $callable)
+    {
+        $route = new Route($path, $callable);
+        $this->routes['GET'][] = $route;
+    }
 
-        public function get($path, $callable)
-        {
-            $route = new Route($path, $callable);
-            $this->routes['GET'][] = $route;
-        }
+    public function post($path, $callable)
+    {
+        $route = new Route($path, $callable);
+        $this->routes['POST'][] = $route;
+    }
 
-        public function post($path, $callable)
-        {
-            $route = new Route($path, $callable);
-            $this->routes['POST'][] = $route;
-        }
-
-        public function run()
-        {
-            if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
-                (new Error404Controller())->render();
-                exit();
-            }
-            foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
-                if ($route->match($this->url)) {
-                    return $route->call();
-                }
-            }
+    public function run()
+    {
+        if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
             (new Error404Controller())->render();
             exit();
         }
+        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
+            if ($route->match($this->url)) {
+                return $route->call();
+            }
+        }
+        (new Error404Controller())->render();
+        exit();
     }
+}
