8dc871868c13f0bc1cefed35195366786c398085
Zeta002 <angeloportelli05@gmail.com>
2023-12-11 14:29:38 +0100

    Restructuration MVC

---
+++ View/pages/homepage.php
@@ -0,0 +1,25 @@
+<nav class="main-nav">
+    <div id="pages-container">
+        <a class="pages-link" href="../pages/abvout">A propos</a>
+        <a class="pages-link" href="../pages/game">Jouer</a>
+        <a class="pages-link" href="../pages/howtoplay">Comment jouer</a>
+        <a class="pages-link" href="../pages/credit">Crédit</a>
+    </div>
+    <a href="https://iut.univ-amu.fr/fr">
+        <img id="logo-iut" src="images/univ-logo.png" alt="logo institut universitaire de technologie">
+    </a>
+</nav>
+<section class="intro-container">
+    <div class="intro">
+        <div id="intro-title-container">
+            <div id="title">
+                <img id="diagram-projet-solid" src="images/diagram-project-solid.svg" alt="projet logo généric"/>
+                <h1>Projet étudiant</h1>
+            </div>
+            <p id="intro-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae distinctio hic impedit iusto maiores placeat reiciendis tenetur vero. A dolore laudantium possimus quas quidem. Numquam porro recusandae totam voluptate!</p>
+        </div>
+    </div>
+</section>
+<section id="content">
+    <h1>Objectifs pédagogique</h1>
+</section>
--- index.php
+++ index.php
@@ -4,24 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAE S3 Projet RV</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="assets/css/stylesheet/style.css">
+    <link rel="stylesheet" href="assets/css/stylesheet/navbar.css">
 </head>
 <body>
     <nav class="main-nav">
         <div id="pages-container">
-            <a class="pages-link" href="../pages/about">A propos</a>
+            <a class="pages-link" href="../pages/abvout">A propos</a>
             <a class="pages-link" href="../pages/game">Jouer</a>
             <a class="pages-link" href="../pages/howtoplay">Comment jouer</a>
             <a class="pages-link" href="../pages/credit">Crédit</a>
         </div>
         <a href="https://iut.univ-amu.fr/fr">
-            <img id="logo-iut" src="images/univ-logo.png" alt="logo institut universitaire de technologie">
+            <img id="logo-iut" src="assets/images/univ-logo.png" alt="logo institut universitaire de technologie">
         </a>
     </nav>
-    <div class="content">
-        <h1>Contenu de la page</h1>
-        <p>Autres éléments...</p>
-    </div>
+    <section class="intro-container">
+        <div class="intro">
+            <div id="intro-title-container">
+                <div id="title">
+                    <img id="diagram-projet-solid" src="assets/images/diagram-project-solid.svg" alt="projet logo généric"/>
+                    <h1>Projet étudiant</h1>
+                </div>
+                <p id="intro-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae distinctio hic impedit iusto maiores placeat reiciendis tenetur vero. A dolore laudantium possimus quas quidem. Numquam porro recusandae totam voluptate!</p>
+            </div>
+        </div>
+    </section>
+    <section id="content">
+        <h1>Objectifs pédagogique</h1>
+    </section>
 </body>
 </html>
