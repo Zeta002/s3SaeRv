8b7239cebc47198aa2f59033ffca7862215e27eb
Zeta002 <angeloportelli05@gmail.com>
2023-12-18 12:01:46 +0100

    test database

--- src/View/pages/About.php
+++ src/View/pages/About.php
@@ -1,1 +1,11 @@
 <h1>Test about</h1>
+
+<?php
+
+    use App\src\Controller\DatabaseTest;
+    use App\src\Database\DatabaseConnexion;
+
+    $db = new DatabaseTest(DatabaseConnexion::getInstance());
+
+    echo $db->displayTest();
+?>
