7478924c4e1df7dce37918d2028fbcf6ddf616de
ROUBAUD-Axel-2225073b <axel.roubaud@etu.univ-amu.fr>
2024-02-20 15:16:48 +0100

    remplissage de la page principale

--- src/View/pages/Homepage.php
+++ src/View/pages/Homepage.php
@@ -14,9 +14,14 @@
 </section>
 <section id="content">
     <h1>Objectifs pédagogique</h1>
-    <p id="pedagog">Répondre aux mieux aux attente du client attribué lié aux projet , travaille en equipe ,<br>
-        mobiliser du mieux possible nos connaissance, faire de la veille informatique</p>
+<p id="pedagog">Nous nous efforçons de répondre au mieux aux attentes du client liées au projet, en travaillant en équipe. Nous mobilisons nos connaissances de la manière la plus efficace possible et nous nous tenons constamment à jour grâce à la veille informatique.</p>
+<p>C'est un projet pédagogique visant à nous améliorer et à découvrir de nouvelles choses. Nous nous efforçons de répondre au mieux aux attentes pour satisfaire au mieux.</p><br>
+<p>Le projet est un jeu sous Unity qui vise à enseigner les bases de l'astronomie à des élèves de fin de primaire et de collège. Nous pouvons observer la rotation de la Terre autour du Soleil et les changements de saison au cours de l'année. <br>
+    À la fin, l'élève doit passer un quiz et répondre aux questions du mieux possible pour évaluer ses connaissances.</p>
 </section>
+</section>
+
+
 
 <?php $test = new \App\src\model\Database\dbTest();
 var_dump($test->test());
