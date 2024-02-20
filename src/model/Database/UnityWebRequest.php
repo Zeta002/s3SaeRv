20220ea8ecd89431c5dab3d668285c14d47561c2
Zeta002 <angeloportelli05@gmail.com>
2024-02-20 10:38:46 +0100

    Continuation UnityWebRequest.php

--- src/model/Database/UnityWebRequest.php
+++ src/model/Database/UnityWebRequest.php
@@ -7,19 +7,20 @@ use PDO;
 class UnityWebRequest
 {
     private PDO $dbPDO;
-    private string $maVariable;
+    private string $ResultaField;
 
     public function __construct(string $maVariable)
     {
         $db = new DatabaseConnexion();
         $this->dbPDO = $db->getPDO();
-        $this->maVariable = $_POST['maVariable'];
+        $this->ResultaField = $_POST['ResultaField'];
     }
 
     public function verify() {
-        if (isset($this->maVariable)) {
+        if (isset($this->ResultaField)) {
+            return true;
             // TODO: Add the logic to verify the variable
-        }
+        } else
+            return false;
     }
-
 }
