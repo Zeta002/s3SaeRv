1458efd6d46d2ba25af51c1d169409685bf883b9
ROUBAUD-Axel-2225073b <axel.roubaud@etu.univ-amu.fr>
2024-02-21 14:35:40 +0100

    correctif des bouton du slider (carroussel de la documentation)

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,19 +1,13 @@
-
 let slideIndex = 1;
-
-
 displaySlide(slideIndex);
-
 
 function changeSlide(n) {
     displaySlide(slideIndex += n);
 }
 
-
 function currentSlide(n) {
     displaySlide(slideIndex = n);
 }
-
 
 function displaySlide(n) {
     let i;
@@ -21,12 +15,8 @@ function displaySlide(n) {
     let dots = document.getElementsByClassName("dot");
     let titles = ["start-title", "loupe-title", "temps-title", "rotation-title", "quizz-title-1", "quizz-title-2", "quizz-title-3"];
 
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -36,26 +26,29 @@ function displaySlide(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 
-    // Hide all titles
     titles.forEach(title => {
         document.getElementById(title).style.display = "none";
     });
 
-    // Display the appropriate title based on the currently displayed slide
     if (slideIndex <= titles.length) {
         document.getElementById(titles[slideIndex - 1]).style.display = "block";
     }
 }
 
+document.querySelector('.prev').addEventListener('click', function() {
+    changeSlide(-1);
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+    changeSlide(1);
+});
 
 window.addEventListener('keydown', function(event) {
     switch (event.key) {
         case "ArrowLeft":
-            // Left arrow key pressed
             changeSlide(-1);
             break;
         case "ArrowRight":
-            // Right arrow key pressed
             changeSlide(1);
             break;
     }
